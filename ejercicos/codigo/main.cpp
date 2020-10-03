cd15116fde65ef96742719e5b0cfa9f265a1412b
rinforma cruz <rfcruz@unah.hn>
2020-10-03 15:03:02 -0600

    exa

--- ejercicos/codigo/main.cpp
+++ ejercicos/codigo/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 #include "startShip.h"
 #include "snake.h"
 
@@ -6,10 +7,47 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    int op;
+	while(op!=4){
+		cout<<"1.*** menu del juego *** "<<endl;
+		cout<<"2. snake "<<endl;
+		cout<<"3. startship "<<endl;
+		cout<<"4. salir "<<endl;
+		cout<<"elija una opcion";
+		cin>>op;
+		
+		switch(op){
+			
+			case 1: 
+				system("cls");// sirve para limpiuar pantalla
+				cout<<"menu de juego "<<endl;
+				system("pause");// sirve para cuando se ejecute el codigo
+				break;
+			case 2: 
+				system("cls");// sirve para limpiuar pantalla
+				cout<<"snake 2"<<endl;
+				system("pause");// sirve para cuando se ejecute el codigo
+				break;
+			case 3: 
+				system("cls");// sirve para limpiuar pantalla
+				cout<<"startship 3"<<endl;
+				system("pause");// sirve para cuando se ejecute el codigo
+				break;
+			case 4: 
+				system("cls");// sirve para limpiuar pantalla
+				cout<<"salir 4"<<endl;
+				system("pause");// sirve para cuando se ejecute el codigo
+				break;
+			
+		}
+	
+}
+
+
     system("cls");
 
-    // starShip();
-    // snake();
+    starShip();
+     snake();
 
     return 0;
 }
